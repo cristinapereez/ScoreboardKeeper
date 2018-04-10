f6c7eeca2058244a77bc81c225a7badc3b999436
cristinapereez <cperez58@mail.csuchico.edu>
2018-04-10 11:36:37 -0700

    Complete lab

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
 {
   int periods;
   int teams;
+  vector<vector<int> > board;
 
   cout<<"How many competitors? ";
   cin>>teams;
@@ -27,9 +28,33 @@ int main()
   }
   else
   {
-   //make scoreboard and fill it with zeros
-  
+   //make scoreboard 
+    board.resize(teams);
+    for(int row = 0; row < board.size(); row++)
+    {
+     board[row].resize(periods);
+    }
+   // traverse through matrix and fill it with zeros
+   int current = 0;
+   for(int row = 0; row < board.size(); row++)
+   {
+    for(int col = 0; col < board[row].size(); col++)
+    {
+        board[row][col] = current;
+    }
+   }
    //once created, display the scoreboard
-  }
+   cout<<"SCOREBOARD"<<endl;
+   for(int row = 0; row < board.size(); row++)
+   {
+     cout<<"Player "<<row+1<<": ";
+     for(int col = 0; col < board[row].size(); col++)
+     {
+//       cout<<"Player "<<row+1;
+       cout<<board[row][col]<<"|";
+     }
+     cout<<endl;
+   }
+  } 
   return 0;
 }
