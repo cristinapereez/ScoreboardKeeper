67d3f9a93e56c2b9d2b1dff29ac2e5ddfc108926
cristinapereez <cperez58@mail.csuchico.edu>
2018-04-12 11:39:08 -0700

    Complete part B & C

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 const int MAX_PERIODS = 10;
@@ -8,10 +9,12 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 void printScoreboard(vector<vector<int> >); //prototype
+int randomBetween(int,int); //prototype for random numbers
 vector<vector<int> > board;
 
 int main()
 {
+  srand((int) time(0));
   int periods;
   int teams;
 //  vector<vector<int> > board;
@@ -45,12 +48,22 @@ int main()
         board[row][col] = current;
     }
    }
-   }
+  }
 printScoreboard(board); //call
+   // traverse through matrix again and fill it with random numbers
+   for(int row = 0; row < board.size(); row++)
+   {
+    for(int col = 0; col < board[row].size(); col++)
+    {
+        board[row][col] = randomBetween(5,5);
+    }
+   }
+ printScoreboard(board); //call it again
+
   return 0;
 }
    //once created, display the scoreboard
-void printScoreboard(vector<vector<int> > grid)
+void printScoreboard(vector<vector<int> > grid) //definition
 {
    cout<<"SCOREBOARD"<<endl;
    for(int row = 0; row < board.size(); row++)
@@ -64,4 +77,20 @@ void printScoreboard(vector<vector<int> > grid)
      cout<<endl;
    }
 } 
+int randomBetween(int first, int second) //definition for random numbers
+{
+  if(first > second)
+  {
+    return second + rand()%(first-second+1);
+  }
+  else if(second > first)
+  {
+  return first + rand()%(second-first+1);
+  }
+}
+
+
+
+
+
 
